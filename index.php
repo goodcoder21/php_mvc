62385f4725568a01692340581628a273724d31bc
goodcoder21 <38079776+goodcoder21@users.noreply.github.com>
2019-09-22 14:54:13 +0300

    online shop start
    Lesson3 php start

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php
 
-//all errors on
-ini_set('display_errors', 1);
+// FRONT CONTROLLER
+
+// 1. Общие настройки
+ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 
-// files turn on
+// 2. Подключение файлов системы
 define('ROOT', dirname(__FILE__));
 require_once(ROOT.'/components/Router.php');
 require_once(ROOT.'/components/Db.php');
 
 
-
-// router run
+// 4. Вызов Router
 $router = new Router();
 $router->run();
-
