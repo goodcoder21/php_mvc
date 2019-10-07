37b1ce8acc8a10ad6b774acf9ece383847f75eb4
goodcoder21 <38079776+goodcoder21@users.noreply.github.com>
2019-10-07 14:47:49 +0300

    added user cabinet

--- controllers/CatalogController.php
+++ controllers/CatalogController.php
@@ -1,14 +1,11 @@
 <?php
 
-include_once ROOT . '/models/Category.php';
-include_once ROOT . '/models/Product.php';
-include_once ROOT . '/components/Pagination.php';
-
 class CatalogController
 {
 
     public function actionIndex()
     {
+
         $categories = array();
         $categories = Category::getCategoriesList();
 
@@ -19,21 +16,23 @@ class CatalogController
 
         return true;
     }
-    
-    public function actionCategory($categoryId,  $page = 1)
+
+    public function actionCategory($categoryId, $page = 1)
     {
-				
         $categories = array();
         $categories = Category::getCategoriesList();
-        
+
         $categoryProducts = array();
         $categoryProducts = Product::getProductsListByCategory($categoryId, $page);
-		
-		$total = Product::getTotalProductsInCategory($categoryId);
-		$pagination = new Pagination($total, $page, Product::SHOW_BY_DEFAULT, 'page-');
-       
+
+        $total = Product::getTotalProductsInCategory($categoryId);
+
+        // Создаем объект Pagination - постраничная навигация
+        $pagination = new Pagination($total, $page, Product::SHOW_BY_DEFAULT, 'page-');
+
         require_once(ROOT . '/views/catalog/category.php');
 
         return true;
     }
+
 }
